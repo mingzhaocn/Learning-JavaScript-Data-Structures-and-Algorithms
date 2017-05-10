820600b85ff1ed14102a4202c2271ab0feb38074
songmingzhao <songmz@songmingzhaodeMacBook-Pro.local>
2017-05-10 15:58:52 +0800

    平衡圆括号

---
+++ stack/algorithm/BalancedParentheses.js
@@ -0,0 +1,35 @@
+/**
+ * 平衡圆括号
+ */
+const Stack = require('../Stack');
+
+function parenthesesChecker(Symbols) {
+  const opens = '{[(',
+    closers = '}])',
+    stack = new Stack();
+  let balanced = true,
+    index = 0;
+  while (index < Symbols.length && balanced) {
+    const curSymbol = Symbols[index];
+    if (opens.indexOf(curSymbol) !== -1) {
+      stack.push(curSymbol);
+    } else {
+      if (stack.isEmpty()) {
+        balanced = false;
+        return;
+      }
+      let top = stack.pop();
+      if (opens.indexOf(top) === closers.indexOf(curSymbol)) {
+        // console.log(`current symbol is ${index} ${curSymbol} matched`)
+      } else {
+        balanced = false;
+      }
+    }
+    index++;
+  }
+  return balanced && stack.isEmpty();
+}
+
+console.log(parenthesesChecker('{([])}')); //true
+console.log(parenthesesChecker('{{([][])}()}')); //true
+console.log(parenthesesChecker('[{()]')); //false
