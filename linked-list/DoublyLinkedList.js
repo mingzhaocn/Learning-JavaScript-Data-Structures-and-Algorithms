558800c1c108e74a476a6d2df6bcb4cff7193a94
songmingzhao <songmz@songmingzhaodeMacBook-Pro.local>
2017-05-27 22:46:38 +0800

    eslint

--- linked-list/DoublyLinkedList.js
+++ linked-list/DoublyLinkedList.js
@@ -4,6 +4,13 @@ module.exports = class DoublyLinkedList {
     this.head = null;
     this.tail = null;
   }
+  insert(position, element) {
+    if (position >= length && position <= this.length) {
+      
+    } else {
+
+    }
+  }
 };
 class Node {
   constructor(element) {
@@ -12,3 +19,4 @@ class Node {
     this.next = null;
   }
 }
+
