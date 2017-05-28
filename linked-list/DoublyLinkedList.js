25bc8d8aa271f4f7c9fb8ec4ab4866d44495ca45
songmingzhao <songmz@songmingzhaodeMacBook-Pro.local>
2017-05-28 09:42:39 +0800

    DoublyLinkedList insert

--- linked-list/DoublyLinkedList.js
+++ linked-list/DoublyLinkedList.js
@@ -1,14 +1,45 @@
-module.exports = class DoublyLinkedList {
+const LinkedList = require('./LinkedList')
+module.exports = class DoublyLinkedList extends LinkedList {
   constructor() {
-    this.length = 0;
-    this.head = null;
+    super();
     this.tail = null;
   }
-  insert(position, element) {
-    if (position >= length && position <= this.length) {
-      
-    } else {
 
+  insert(position, element) {
+    if (position >= 0 && position <= this.length) {
+      const node = new Node(element);
+      let current = this.head,
+        previous,
+        next,
+        index = 0;
+      if (position === 0) {
+        if (!this.head) {
+          this.head = node;
+          this.tail = node;
+        } else {
+          node.next = current;
+          current.prev = node;
+          this.head = node;
+        }
+      } else if (position === this.length) {
+        current = this.tail;
+        current.next = node;
+        node.prev = current;
+        this.tail = node;
+      } else {
+        while (index++ < position) {
+          previous = current;
+          current = current.next;
+        }
+        node.next = current;
+        current.prev = node;
+        node.prev = previous;
+        previous.next = node;
+      }
+      this.length++;
+      return true;
+    } else {
+      return false;
     }
   }
 };
--- linked-list/index.js
+++ linked-list/index.js
@@ -1,22 +1,31 @@
 const LinkedList = require('./LinkedList');
+const DoublyLinkedList = require('./DoublyLinkedList');
 
-const linkedList = new LinkedList();
-linkedList.append(1);
-linkedList.append(5);
-linkedList.append(10);
-console.log('append:', linkedList.toString())//1,5,10
+const doublyLinkedList = new DoublyLinkedList();
+doublyLinkedList.insert(0, 3);
+doublyLinkedList.insert(0, 5);
+doublyLinkedList.insert(0, 4);
+doublyLinkedList.insert(1, 1);
+console.log(doublyLinkedList.toString());
+// const linkedList = new LinkedList();
+// linkedList.append(1);
+// linkedList.append(5);
+// linkedList.append(10);
+// console.log('append:', linkedList.toString())//1,5,10
 
-// linkedList.insert(8, 2)
-console.log('insert:', linkedList.toString())//
+// // linkedList.insert(8, 2)
+// console.log('insert:', linkedList.toString())
 
-linkedList.removeAt(2)
-console.log('removeAt:', linkedList.toString())//
+// linkedList.removeAt(2)
+// console.log('removeAt:', linkedList.toString())
 
-linkedList.insert(3, 1)
-console.log('insert:', linkedList.toString())//
-console.log('indexOf:', linkedList.indexOf(3))//
+// linkedList.insert(3, 1)
+// console.log('insert:', linkedList.toString())
+// console.log('indexOf:', linkedList.indexOf(3))
 
-linkedList.remove(1)
-console.log('remove:', linkedList.toString())//
+// linkedList.remove(1)
+// console.log('remove:', linkedList.toString())
+
+
 
 
